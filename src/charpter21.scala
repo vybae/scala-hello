f7be53205f5484d5ad30b05a1ff3dd9dad308ac0
lujunya <miss24@qq.com>
2016-06-03 11:13:30 +0800

    charpter 21 code comments

--- src/charpter21.scala
+++ src/charpter21.scala
@@ -4,7 +4,10 @@ import scala.io.Source
 
 object charpter21 extends App {
   /*
-   * question 1
+   * 21.1
+   * ->的工作原理是什么? 
+   * 或者说, "Hello" -> 42 和 42 -> "Hello"怎么会和对偶扯上关系?
+   * 
    * "hello" -> 43会转化成("hello",43)是由于隐式转化的原因
    * ArrowAssoc[A]内定义了名为 -> [B](y: B): Tuple2[A, B]的方法
    * 当使用到类似于 "hello" -> 43 的实例时
@@ -12,7 +15,14 @@ object charpter21 extends App {
    */
 
   /*
-   * question 2 & 3
+   * 21.2 & 21.3
+   * 2)
+   *   定义一个操作符+%, 将一个给定的百分比添加到某个值.
+   *   举例来说,120 +% 10 等于132.
+   *   提示由于操作符是方法, 而不是函数, 你需要提供一个implicit
+   * 3)
+   *   定义一个!操作符, 计算某个整数的阶乘. 举例来说, 5!应得到120.
+   *   你将会需要一个经过丰富的类和一个隐式转换.
    */
   case class MyInt(v: Int) {
     def +%(op: Int) = v + v / 100.0 * op
@@ -28,13 +38,15 @@ object charpter21 extends App {
   //  Predef println (5!)
 
   /*
-   * question4
-   * Read in aString askingFor "Your Name" and anInt askingFor "Your age" and aDouble askingFor "Your Weight"
+   * 21.4
+   * 有些人很喜欢那些读起来隐约像英语句子的流利API.
+   * 创建一个这样的API, 用来从控制台读取整数、浮点数以及字符串.
+   * 例如:
+   *   Read in aString askingFor "Your Name" and anInt askingFor "Your age" and aDouble askingFor "Your Weight"
    */
   def question4() {
     Read() in aString askingFor "Your Name" and anInt askingFor "Your age" and aDouble askingFor "Your Weight"
   }
-
   class Read {
     def in(op: TypeOP): Read = this
     def and(op: TypeOP): Read = this
@@ -47,8 +59,10 @@ object charpter21 extends App {
   case object aDouble extends TypeOP {}
 
   /*
-   * question5
-   * smaller(Fraction(1,3), Fraction(3,1))
+   * 21.5
+   * 提供执行下述运算所需要的代码:
+   *   smaller(Fraction(1,3), Fraction(3,1))
+   * 给出一个扩展自Ordered[Fraction]的RichFraction类
    */
   case class Fraction(x: Int, y: Int) {
     def getVal() = x / y
@@ -71,19 +85,20 @@ object charpter21 extends App {
   //  Predef println smaller(Fraction(1, 2), Fraction(1, 3))
 
   /*
-   * question 6 
+   * 21.6
+   * 比较java.awt.Point类的对象, 按词典顺序比较(即依次比较x坐标和y坐标的值)
    */
   class PointOrdering6 extends java.awt.Point with Ordered[java.awt.Point] {
     def compare(that: java.awt.Point): Int =
-      if (this.x > that.x) 1
-      else if (this.x < that.x) -1
-      else if (this.y > that.y) 1
-      else if (this.y < that.y) -1
-      else 0
+      if (this.x > that.x || (this.x == that.x && this.y > that.y)) 1
+      else if (this.x == that.x && this.y == that.y) 0
+      else -1
   }
 
   /*
-   * question 7
+   * 21.7
+   * 继续前一个练习, 根据两个点到原点的距离进行比较.
+   * 你如何在两种排序之间切换?
    */
   class PointOrdering7 extends java.awt.Point with Ordered[java.awt.Point] {
     def compare(that: java.awt.Point): Int =
@@ -94,7 +109,14 @@ object charpter21 extends App {
   type d = Int =:= AnyVal
   
   /*
-   * question 9
+   * 21.8
+   * 在REPL中使用implicitly命令来召唤隐式对象. 你得到了哪些对象?
+   */
+  
+  /*
+   * 21.9
+   * 在Predef.scala中查找=:=对象,解释它的的工作原理
+   * 
    * 例如 
       def getFirstChar[T](arg: T)(implicit ev: T =:= String) = ev(arg).head
    *  事先编译器并不知道参数arg是什么类型的, 但由ev参数将其隐式地转化成了String, 就可以调用head方法了
@@ -102,7 +124,10 @@ object charpter21 extends App {
   def getFirstChar[T](arg: T)(implicit ev: T =:= String) = ev(arg).head
     
   /*
-   * question 10
+   * 21.10
+   * 表达式"abc".map(_.toUpper)的结果是一个String, 
+   * 但"abc".map(_.toInt)的结果是一个Vector.
+   * 为什么会这样?
    */
   val r1 = "abc".map(_.toUpper)
   val r2 = "abc".map(_.toInt)
